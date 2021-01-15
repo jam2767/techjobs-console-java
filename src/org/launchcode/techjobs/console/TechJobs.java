30eb2dffa56b1037a522430e54dd4bc0c1d75398
Joshua Mantooth <joshua.mantooth@joshua.mantooth>
2021-01-14 19:19:30 -0600

    implemented printJobs to display jobs and display a message if no jobs are found

--- src/org/launchcode/techjobs/console/TechJobs.java
+++ src/org/launchcode/techjobs/console/TechJobs.java
@@ -110,7 +110,13 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
-
-        System.out.println("printJobs is not implemented yet");
+        if(someJobs.size() > 0) {
+        for (Integer i = 0; i < someJobs.size(); i++) {
+            someJobs.get(i).forEach((k, v) -> System.out.println(k + ": " + v));
+            System.out.println("****");
+            //for (Integer j = 0; j < )
+        }
+        //System.out.println("printJobs is not implemented yet");
+    } else System.out.println("No matches found.");
     }
 }
